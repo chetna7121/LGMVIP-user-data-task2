e0a202c47380202ba67322a8520baf2f4e92d2e3
Chetna <mchetna28@gmail.com>
2023-07-22 21:28:54 +0530

    hd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-
+import logo from './logo.png';
 import React from 'react';
 import './Navbar.css';
 
 const Navbar = ({ loadUsers }) => {
   return (
     <div className='Header'>
+       <img src={logo} alt="logo" />
       <div className="button-container">
-        <button className="data-button" onClick={loadUsers}>Get Data</button>
+        <button className="data-button" onClick={loadUsers}>Get Users</button>
       </div>
     </div>
   );
